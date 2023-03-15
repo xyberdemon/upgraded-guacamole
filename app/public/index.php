eafbb68ccfa2a95fc38ac9881cdd159ff450430a
Dmitrii Bainov <xyberdemon@gmail.com>
2023-03-15 14:36:59 +0100

    add sample db and mysqli extension

--- app/public/index.php
+++ app/public/index.php
@@ -1,2 +1,32 @@
 <?php
-phpinfo();
+// Database credentials
+$servername = "mysql";
+$username = "example-user";
+$password = "my_cool_secret";
+$dbname = "samplevideo_db";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+// SQL query to retrieve data from table
+$sql = "SELECT user_id, first_name, last_name FROM user_details";
+$result = $conn->query($sql);
+
+// Check if any rows were returned
+if ($result->num_rows > 0) {
+    // Output data of each row
+    while($row = $result->fetch_assoc()) {
+        echo "user_id: " . $row["user_id"]. " - First Name: " . $row["first_name"]. " - Last Name: " . $row["last_name"]. "<br>";
+    }
+} else {
+    echo "0 results";
+}
+
+// Close connection
+$conn->close();
+?>
